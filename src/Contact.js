d3768a4c741900e10a0066027c73e60bc203e9e7
alperenatikhan <alperenmyung@gmail.com>
2021-06-26 14:28:59 +0200

    2 files modified

--- src/Contact.js
+++ src/Contact.js
@@ -1,92 +1,102 @@
 import React from 'react';
-import FadeIn from "react-fade-in";
+import FadeIn from 'react-fade-in';
 
 export default function Contact() {
   return (
     <section className="main" id="contact-page">
-     
-      <div className="main-wrapper" id="contact-wrapper"style={{display: "flex", flexDirection:"column", justifyContent:"center", alignItems:"center", backgroundColor:"whitesmoke"}}>
-      <FadeIn transitionDuration="1500"><img src="https://i.imgur.com/uCRljQC.png" alt="" style={{width:"40vh", marginBottom: "1rem", borderRadius:"1rem"}}/> </FadeIn>
+      <div
+        className="main-wrapper"
+        id="contact-wrapper"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'whitesmoke'
+        }}
+      >
+        <FadeIn transitionDuration="1500">
+          <img
+            src="https://i.imgur.com/uCRljQC.png"
+            alt=""
+            style={{
+              width: '40vh',
+              marginBottom: '1rem',
+              borderRadius: '1rem'
+            }}
+          />{' '}
+        </FadeIn>
 
-
-        <div className="contact-text-space" style={{marginBottom:"0.7rem"}}>
+        <div className="contact-text-space" style={{ marginBottom: '0.7rem' }}>
           <FadeIn transitionDuration="3000">
-          <table>
-            <tr>
-              {' '}
-              <td>
-                {' '}
-                <i class="fab fa-github" />
-                <strong>Github</strong>{' '}
-              </td>{' '}
-              <td>
-                <a href="http://github.com/alperenatikhan/" target="_blank">
-                  alperenatikhan
-                </a>
-              </td>{' '}
-            </tr>
-            <tr>
-              {' '}
-              <td>
-                {' '}
-                <i class="fab fa-facebook" />
-                <strong>Facebook</strong>{' '}
-              </td>{' '}
-              <td>
-                {' '}
-                <a
-                  href="https://pl-pl.facebook.com/alperen.atik"
-                  target="_blank"
-                >
-                  Alperen Atik
-                </a>{' '}
-              </td>{' '}
-            </tr>
-            <tr>
-              {' '}
-              <td>
-                <i class="fab fa-whatsapp" />
-                <strong>WhatsApp</strong>{' '}
-              </td>{' '}
-              <td>
-                {' '}
-                <a href="https://wa.me/48731166245" target="_blank">
-                  {' '}
-                  0048 731 166 245
-                </a>
-              </td>{' '}
-            </tr>
-            <tr>
-              {' '}
-              <td>
-                <i class="fas fa-at" />
-                <strong>E-mail</strong>{' '}
-              </td>{' '}
-              <td>
-                <a href="mailto:alperenmyung@gmail.com" target="_blank">
-                  {' '}
-                  alperenmyung@gmail.com
-                </a>
-              </td>{' '}
-            </tr>
-            <tr>
-              {' '}
-              <td>
-                <i class="fa fa-skype" />
-                <strong>Skype</strong>{' '}
-              </td>{' '}
-              <td>
-                {' '}
-                <a href="http://join.skype.com/BPFTsBYJjDMs" target="_blank">
-                  alperen.myung{' '}
-                </a>
-              </td>{' '}
-            </tr>
-          </table>
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <i class="fab fa-github" />
+                    <strong>Github</strong>
+                  </td>
+                  <td>
+                    <a href="http://github.com/alperenatikhan/" target="_blank">
+                      alperenatikhan
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <i class="fab fa-facebook" />
+                    <strong>Facebook</strong>
+                  </td>
+                  <td>
+                    <a
+                      href="https://pl-pl.facebook.com/alperen.atik"
+                      target="_blank"
+                    >
+                      Alperen Atik
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <i class="fab fa-whatsapp" />
+                    <strong>WhatsApp</strong>
+                  </td>
+                  <td>
+                    <a href="https://wa.me/48731166245" target="_blank">
+                      0048 731 166 245
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <i class="fas fa-at" />
+                    <strong>E-mail</strong>
+                  </td>
+                  <td>
+                    <a href="mailto:alperenmyung@gmail.com" target="_blank">
+                      alperenmyung@gmail.com
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <i class="fa fa-skype" />
+                    <strong>Skype</strong>
+                  </td>
+                  <td>
+                    <a
+                      href="http://join.skype.com/BPFTsBYJjDMs"
+                      target="_blank"
+                    >
+                      alperen.myung
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </FadeIn>
         </div>
       </div>
-     
     </section>
   );
 }
