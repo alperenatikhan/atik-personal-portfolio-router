419c028d7608ce46ae36f7847adbaec8e90b6d01
alperenatikhan <alperenmyung@gmail.com>
2021-06-28 18:15:23 +0200

    1 file modified

--- src/ClickContactForm.js
+++ src/ClickContactForm.js
@@ -11,40 +11,53 @@ export default function ClickContactForm() {
       }}
     >
       {' '}
-      <form>
+      <form action="https://formsubmit.co/alperenmyung@gmail.com" method="POST">
         {' '}
         <table className="contact-table">
           <tr className="contact-row">
             <td className="label-cell">
-              <label for="exampleInputEmail1">Name: </label>{' '}
+              <label for="contact-name">Name: </label>{' '}
             </td>{' '}
             <td>
-              <input type="text" placeholder="Type Your Name" required />{' '}
+              <input
+                type="text"
+                name="contact-name"
+                placeholder="Type Your Name"
+                required
+              />{' '}
             </td>
           </tr>
           <tr className="contact-row">
             <td className="label-cell">
-              <label for="exampleInputEmail1">E-mail: </label>
+              <label for="contact-email">E-mail: </label>
             </td>
             <td>
-              <input placeholder="Type Your E-mail" required />{' '}
+              <input
+                type="e-mail"
+                name="contact-email"
+                placeholder="Type Your E-mail"
+                required
+              />{' '}
             </td>
           </tr>
           <tr className="contact-row">
             <td className="label-cell">
-              <label for="exampleInputEmail1">Message: </label>{' '}
+              <label for="contact-message">Message: </label>{' '}
             </td>{' '}
             <td>
-              <textarea placeholder="Type Your Message" required />{' '}
+              <textarea
+                name="contact-message"
+                placeholder="Type Your Message"
+                required
+              />{' '}
             </td>
-           
-            </tr>
+          </tr>
 
-            <tr>
-              <td colspan="2" ><button style={{margin:"1rem 30%"}}>Send Message</button></td>
-            </tr>
-    
-        
+          <tr>
+            <td colspan="2">
+              <button style={{ margin: '1rem 30%' }}>Send Message</button>
+            </td>
+          </tr>
         </table>
       </form>{' '}
     </div>
